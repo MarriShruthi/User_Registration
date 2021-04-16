602bc3eed3236d3777f37576aeb8f6cdca320fd6
Shruthi <shruthimarri97@gmail.com>
2021-04-16 21:31:42 +0530

    [Shruthi] : Added User Registration First Name

---
+++ UserReg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration.FName();
+        }
+    }
+}
---
+++ UserReg/UserRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace UserReg
+{
+    class UserRegistration
+    {
+        public static void FName()
+        {
+            string regex = "[A-Z]{1}[a-z]{3,10}$";
+            Console.WriteLine("Enter your First Name :");
+            string fName = Console.ReadLine();
+            Regex rg = new Regex(regex);
+          //  Match result = Regex.Match(fName, regex);
+            bool result1 = rg.IsMatch(fName);
+            if (result1 == true)
+            {
+                Console.WriteLine("valid Input");
+                Console.WriteLine("your first name : " + fName);
+            }
+            else
+            {
+                Console.WriteLine("invalid input ");
+            }
+        }
+    }
+}
+
